a28d923b64639f40c65cc2201bd9693104578888
Tran Huy Thang <tranhuythang9999@gmail.com>
2025-03-04 11:08:40 +0700

    Enhance JwtMiddleware to validate user existence and password update timing; integrate IUserRepository for user retrieval and add checks for last password update against token issuance

--- WebApplicationCQRS/Infrastructure/Middleware/JwtMiddleware.cs
+++ WebApplicationCQRS/Infrastructure/Middleware/JwtMiddleware.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
-
+using WebApplicationCQRS.Domain.Interfaces;
 using WebApplicationCQRS.Infrastructure.Security;
 
 namespace WebApplicationCQRS.Infrastructure.Middleware
@@ -8,11 +8,13 @@ namespace WebApplicationCQRS.Infrastructure.Middleware
     {
         private readonly ILogger<JwtMiddleware> _logger;
         private readonly IJwtService _jwtService;
+        private readonly IUserRepository _userRepository;
 
-        public JwtMiddleware(ILogger<JwtMiddleware> logger, IJwtService jwtService)
+        public JwtMiddleware(ILogger<JwtMiddleware> logger, IJwtService jwtService, IUserRepository userRepository)
         {
             _logger = logger;
             _jwtService = jwtService;
+            _userRepository = userRepository;
         }
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
@@ -44,6 +46,38 @@ namespace WebApplicationCQRS.Infrastructure.Middleware
                 await context.Response.WriteAsync("Unauthorized");
                 return;
             }
+
+            var user = await _userRepository.GetUserById(int.Parse(userId));
+            if (user == null)
+            {
+                _logger?.LogWarning("Unauthorized request - User not found.");
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            var lastPasswordUpdateString = tokenInfo["lastPasswordUpdate"];
+            if (!DateTime.TryParse(lastPasswordUpdateString, out var lastPasswordUpdate))
+            {
+                _logger?.LogWarning("Unauthorized request - Invalid lastPasswordUpdate format.");
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            long lastPasswordChangedAtTimestamp = new DateTimeOffset(user.LastPasswordChangedAt).ToUnixTimeSeconds();
+            long lastPasswordUpdateTimestamp = new DateTimeOffset(lastPasswordUpdate).ToUnixTimeSeconds();
+
+            if (lastPasswordChangedAtTimestamp > lastPasswordUpdateTimestamp)
+            {
+                _logger?.LogWarning("Unauthorized request - Password changed after token issued.");
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync(
+                    $"Unauthorized {lastPasswordChangedAtTimestamp} : {lastPasswordUpdateTimestamp}");
+                return;
+            }
+
+
             context.Items["userID"] = userId;
 
             await next(context);
